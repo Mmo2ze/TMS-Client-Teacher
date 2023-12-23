d78c852c718818d759639f0c0d8cb68072b02526
kindreklamar <139592450+kindreklamar@users.noreply.github.com>
2023-12-23 09:19:48 +0300

    df

--- app/registration/page.jsx
+++ app/registration/page.jsx
@@ -1,36 +1,9 @@
-"use client"
-import React, {useState , useRef } from 'react'
-import Scanner from '../ui/scanner/Scanner'
-import { endLodingToast, lodingToast, sendToast } from "../func/toast";
-import axios from "../config/axiosconfigClient"
+
 const page = () => {
-  const [student, setStudent] = useState({});
+ 
 
-  const scaner = useRef(null);
-
-
-  function success(result) {
-    scaner.current.pause();
-    axios.get(`api/Student/${result}`)
-      .then((response) => {
-        setStudent(response.data);
-      })
-      .catch((error) => {
-        setTimeout(() => {
-          scaner.current.resume();
-        }, 2000);
-
-        if (
-          error.response.data.title == "One or more validation errors occurred."
-        )
-          sendToast("Invalid QR ", "error");
-        else if (error.response.status == 404) {
-          sendToast("Student Not found", "error");
-        } else {
-          sendToast("Please, try aggain later....", "error");
-        }
-      });
-  }
+ 
+  
 
 
 
@@ -39,7 +12,7 @@ const page = () => {
     <div className='pt-20 page'>    
       <>
           <div className="main-conten">
-          <Scanner success={success} scanner={scaner} />
+          scanner
           </div>
         </>
     </div>
