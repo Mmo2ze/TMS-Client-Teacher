61245117d19f0254980f7df475d5fa25563bbaa6
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-01-14 22:05:49 +0200

    test scanner

--- app/registration/page.jsx
+++ app/registration/page.jsx
@@ -1,16 +1,45 @@
+"use client";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect, useRef } from "react";
+import Scanner from "../ui/scanner/Scanner";
 
 const page = () => {
+  const scaner = useRef(null);
 
+  function success(result) {
+    scaner.current.pause();
+    request(`api/Student/${result}`)
+      .then((response) => {
+        setStudent(response.data);
+        setShowpop(true);
+      })
+      .catch((error) => {
+        setTimeout(() => {
+          scaner.current.resume();
+        }, 2000);
+
+        if (
+          error.response.data.title == "One or more validation errors occurred."
+        )
+          sendToast("Invalid QR ", "error");
+        else if (error.response.status == 404) {
+          sendToast("Student Not found", "error");
+        } else {
+          sendToast("Please, try aggain later....", "error");
+        }
+      });
+  }
 
   return (
-    <div className='pt-20 page'>    
-      <>
-          <div className="main-conten">
-          scanner
-          </div>
-        </>
-    </div>
-  )
-}
+    <div className="add-page">
+      <ToastContainer />
 
-export default page
+        <div className="main-content">
+          <Scanner success={success} scanner={scaner} />
+         
+        </div>
+    </div>
+  );
+};
+
+export default page;
--- app/ui/scanner/Scanner.jsx
+++ app/ui/scanner/Scanner.jsx
@@ -1,36 +1,8 @@
-import React, { useEffect } from 'react';
-import { Html5QrcodeScanner } from 'html5-qrcode';
+'use client'
+import React, { useEffect, useRef } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 function Scanner({ success, scanner }) {
-
-  window.addEventListener("blur",
-    () => {
-      try {
-        var x = document.getElementById('html5-qrcode-button-camera-stop');
-        if (x){
-          x.click();
-        }      }
-      catch (e) {
-        console.log("blure")
-        window.location.reload();
-            }
-    }
-  )
-
-  window.addEventListener("focus",
-    () => {
-      try {
-        
-        var x = document.getElementById('html5-qrcode-button-camera-start');
-        if (x){
-          x.click();
-        }
-      }
-      catch (e) {
-        console.log("focus")
-        window.location.reload();      }
-    }
-  )
   useEffect(() => {
     scanner.current = new Html5QrcodeScanner("reader", {
       qrbox: {
@@ -44,13 +16,9 @@ function Scanner({ success, scanner }) {
     return () => {
       scanner.current.clear();
     };
-  }, []); 
+  }, []); // إزالة الاعتمادات من الـ useEffect
 
-
-  return (
-    <div id="reader"></div>
-  )
-    ;
+  return <div id="reader"></div>;
 }
 
 export default Scanner;
