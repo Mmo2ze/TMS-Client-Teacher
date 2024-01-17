3bdd35252d3cd149bbcce198178295adbcc8f175
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-01-18 01:40:01 +0200

    update order

--- app/cards/[id]/page.jsx
+++ app/cards/[id]/page.jsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from "react";
 import axios from "../../config/axiosconfigClient"
 import DeleteIcon from '@mui/icons-material/Delete';
+import {ToastContainer} from "react-toastify";
+import {sendToast} from "@/app/func/toast";
+import "react-toastify/dist/ReactToastify.css";
 function Page(props){
 
 
@@ -14,8 +17,37 @@ function Page(props){
             console.log(err);
         })
     },[])
+
+
+    function UpdateOrder () {
+        if(cards.length == 0 ){
+            sendToast("لا يمكنك حفظ البيانات فارغة ,يمكنك الغاء الطلب بدلا من ذلك","error");
+            return;
+        }
+        let data = []
+        cards.map((card)=>{
+            data.push(card.privateId)
+        })
+        axios.put(`api/Teacher/Cards/${props.params.id}`,data)
+        .then(response=>{
+            sendToast("تم تحديث البيانات بنجاح","success")
+            setTimeout(() =>             window.location.href = "/cards"
+                , 2000);
+        }).catch(err=>{
+            sendToast("حدث خطأ ما","error");
+        })
+    }
+
+    function removeStuent (student) {
+            setCards((prevArray) => prevArray.filter((obj) => obj.privateId !== student.privateId));
+    }
+
     return(
         <div className="pt-20 px-4  ">
+            <ToastContainer/>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" onClick={UpdateOrder}>
+                حفظ التغيرات
+            </button>
          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-white">
           <thead className="text-base text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
@@ -35,20 +67,20 @@ function Page(props){
             </tr>
           </thead>
           <tbody>
-            {cards.map((card) => (
+            {cards.map((student) => (
 
               <tr className="bg-white border-b text-base dark:bg-gray-800 dark:border-gray-700 text-white">
               <td className="px-6 py-4 text-color-red cursor-pointer">
-                <DeleteIcon/>
+                <DeleteIcon onClick={()=>removeStuent(student)}/>
               </td>
               <td className="px-6 py-4">
-              {card.name}
+              {student.name}
               </td>
               <td className="px-6 py-4">
-              {card.privateId}
+              {student.privateId}
               </td>
               <td className="px-6 py-4">
-              {card.classId}
+              {student.classId}
               </td>
             </tr>
                 ))}
