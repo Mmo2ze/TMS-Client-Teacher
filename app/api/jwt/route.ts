dda2667f161605bb2de5f39c67d65039edbe1677
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-01-20 22:49:55 +0200

    TRY FIX CROS #4

--- app/api/jwt/route.ts
+++ app/api/jwt/route.ts
@@ -1,15 +1,52 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import type { NextRequest } from 'next/server'
+
 import axios from 'axios'
 
 
 export async function GET(req:NextRequest) {
 
+    const requestHeaders = new Headers(req.headers)
+    requestHeaders.set('Access-Control-Allow-Origin', '*')
+    requestHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    requestHeaders.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    const response = NextResponse.next({
+        request: {
+            // New request headers
+            headers: requestHeaders,
+        },
+    })
+
+    // Set a new response header `x-hello-from-middleware2`
+    response.headers.set('x-hello-from-middleware2', 'hello')
     let token = cookies().get("sesstion");
     if(token == null || token.value == undefined || token.value == ""){
-        return NextResponse.json({ message: "not authorized" },{status:401});
+
+       // return NextResponse.json({ message: "not authorized" },{status:401});
+        let body = JSON.stringify({message:"not authorized"})
+        return new Response( body, {
+
+            status: 401,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            }
+        });
     }
-    return NextResponse.json({ token:token.value });
+
+    let body = JSON.stringify({token:token.value})
+    return new Response( body, {
+
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
+    });
+    // return NextResponse.json({ token:token.value },{status:200}, );
 }
 export async function DELETE(req:NextRequest) {
 
