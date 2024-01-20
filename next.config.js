72be454b93425cecfdf9a4bd768c90a60ce8770c
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-01-20 22:31:06 +0200

    TRY FIX CROS #3

--- next.config.js
+++ next.config.js
@@ -6,26 +6,26 @@
                 {
                     source: '/(.*)?', // Matches all pages
                     headers: [
-                        {
-                            key:"Access-Control-Allow-Origin",
-                            value:"*",
-                        },
+                        { key: "Access-Control-Allow-Credentials", value: "true" },
                         {
                             key: 'X-Frame-Options',
                             value: 'DENY',
-                        }
-                    ]
+                        },
+                        { key: "Access-Control-Allow-Origin", value: "*" },
+                        {
+                            key: "Access-Control-Allow-Methods",
+                            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                        },
+                        {
+                            key: "Access-Control-Allow-Headers",
+                            value:
+                                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                        },
+                    ],
                 }
             ]
         }
-        ,  async rewrites() {
-            return [
-                {
-                    source: "/api/:path*",
-                    destination: 'https://tass.ist/:path*',
-                },
-            ]
-        },
+
     }
 
     module.exports = nextConfig
