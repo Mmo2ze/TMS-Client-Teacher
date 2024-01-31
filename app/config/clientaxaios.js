c0f651f3222a78c99d24c5af912e77c278716aee
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-01-31 22:23:17 +0200

    fix clientaxaios.js

--- app/config/clientaxaios.js
+++ app/config/clientaxaios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Next we make an 'instance' of it
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://www.tass.ist/",
   headers: {
     accept: "*/*",
     "Content-Type": "application/json",
