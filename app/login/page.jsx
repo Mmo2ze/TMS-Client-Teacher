d84e93a51e112b5a83e0a2f2e1870a07ad2031fa
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-07-19 17:13:03 +0300

    fix : ui

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -137,7 +137,7 @@ const page = () => {
             </div>
           </section>
         ) : (
-          <section className=" w-[70%] top-[25%] pt-20 bg-gray-900 text-end left-[15%] absolute 2sm:top-[18%] 2sm:left-5 z-20 2sm:w-[90%]">
+          <section className=" w-[70%] top-[25%] pt-20  text-end left-[15%] absolute 2sm:top-[18%] 2sm:left-5 z-20 2sm:w-[90%]">
             <div className="flex direction-r flex-col items-center justify-center px-6 py-8 mx-auto md:h-1/2 lg:py-0">
               <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
